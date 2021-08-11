515755d71bb6fc84444eda0dc6b4e9a40b9581f9
shubhamborke7778 <shubham.borke.17@mountblue.tech>
2021-08-11 12:23:45 +0530

    Problem2 file

---
+++ problem2.js
@@ -0,0 +1,4 @@
+const result2 = function problem2(inventory){
+    return "Last car is a " + inventory[inventory.length-1].car_make + " " + inventory[inventory.length-1].car_model;
+}
+export {result2};
