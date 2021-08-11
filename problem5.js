0f9c57effaed0e3301404c84c58cb1430f3fa65b
shubhamborke7778 <shubham.borke.17@mountblue.tech>
2021-08-11 12:30:20 +0530

    Problem5 file

---
+++ problem5.js
@@ -0,0 +1,10 @@
+const result5 = function problem5(inventory){
+    let arr = [];
+    for(let i = 0; i < inventory.length; i++){
+        if(inventory[i].car_year < 2000){
+            arr.push(inventory[i].car_year);
+        }
+    }
+    return arr;
+}
+export {result5};
